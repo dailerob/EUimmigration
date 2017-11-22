5169393ca56eb56a12fc80ee998cdc4f37e0cbd5
Roberto Dailey <dailey.roberto@utexas.edu>
2017-11-22 17:48:01 -0600

    methods to remove fixed effects and nans working and implemented

--- Load_data_sets.py
+++ Load_data_sets.py
@@ -17,6 +17,7 @@ def formatElement(unformated):
             return np.NaN
         return float(pulledFloats[0])
 
+#pulls the region code for element
 def pullRegion(rowVal):
     return rowVal[rowVal.rfind(',')+1:]
 
@@ -39,14 +40,46 @@ def filter_unemployment():
 
     return df
 
+#isolates statistics for just the NUTS-2 regions
 def isolate_Nuts_2(df):
     removeList = []
 
     for i in range(df.shape[0]):
-        currentIndex = float(re.findall("\\d+(?:\\.\\d+)?", str(df.index[i])))
-        if currentIndex < 9 or currentIndex >100:
+        floatsFound = re.findall("\\d+(?:\\.\\d+)?", str(df.index[i]))
+        if len(floatsFound)==0:
+            currentIndex = 0
+        else:
+            currentIndex = float(floatsFound[0])
+
+        if currentIndex < 10 or currentIndex >99:
             removeList.append(i)
     df = df.drop(df.index[removeList])
+
+    return df
+
+#sets the nans to the regional average
+def removeNaNs(df):
+    for i in range(df.shape[0]):
+        rowAverage = np.mean(df.iloc[i].dropna())
+        df.iloc[i] = df.iloc[i].fillna(rowAverage)
+
+    return df
+
+def removeFixedEffects(df):
+    for i in range(df.shape[0]):
+        rowAverage = np.mean(df.iloc[i])
+        df.iloc[i] = df.iloc[i] - rowAverage
+
+    for i in range(len(df.columns)):
+        columnAverage = np.mean(df[df.columns[i]])
+
+        df[df.columns[i]] = df[df.columns[i]] - columnAverage
+
+    return df
+
+
+
+
 
 
 
